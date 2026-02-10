ce8787bcc263d4537bb98befc17cd699db5f24d6
Youssef-Remah <yremah8@gmail.com>
2026-02-10 21:21:40 +0200

    add register and login endpoints to AuthController

---
+++ eCommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using eCommerce.Core.DTOs;
+using eCommerce.Core.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+
+        public AuthController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            if (request is null) return BadRequest("Invalid Registeration Data");
+
+            var response = await _usersService.RegisterAsync(request);
+
+            if (response is null || !response.Success) return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if(request is null) return BadRequest("Invalid Login Data");
+
+            var response = await _usersService.LoginAsync(request);
+
+            if(response is null || !response.Success) return Unauthorized(response);
+
+            return Ok(response);
+        }
+    }
+}
