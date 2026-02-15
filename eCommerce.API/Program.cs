87b5a7b401c959aa2b7a40e99073c747085da307
Youssef-Remah <yremah8@gmail.com>
2026-02-15 12:34:33 +0200

    add swagger for API docs

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -9,15 +9,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure();
 builder.Services.AddCore();
 builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
+
 builder.Services.AddControllers().AddJsonOptions(options => 
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
+builder.Services.AddCors(coreOptions =>
+{
+    coreOptions.AddDefaultPolicy(policyBuilder =>
+    {
+        policyBuilder.WithOrigins("http://localhost:4200")
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
